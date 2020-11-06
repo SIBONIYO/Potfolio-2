a18b376104ab0495d6f497bc327f7b57026b5b75
unknown <siboniyojeaclaude@gmail.com>
2020-11-06 21:06:14 +0200

    6th Nov 2020 T

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
-repo_token: zN6mkoPGj2n3VJDBC0H9yErmAOwPNZw5a
+
 
 [![Build Status](https://travis-ci.org/SIBONIYO/Potfolio-2.svg?branch=main)](https://travis-ci.org/SIBONIYO/Potfolio-2)
 
 [![Coverage Status](https://coveralls.io/repos/github/SIBONIYO/Potfolio-2/badge.svg?branch=main)](https://coveralls.io/github/SIBONIYO/Potfolio-2?branch=main)
-
-gem 'coveralls', require: false
--- Routes/Posts.js
+++ Routes/Posts.js
@@ -7,21 +7,97 @@ const onePost = require('./onePost');
 
 
 const swaggerJsDoc = require('swagger-jsdoc');
-const swaggerUI = require('swagger-ui-express');
+const swaggerUi = require('swagger-ui-express');
 const { deleteOne } = require("../models/postComment");
 
-// SWAGGER
+// SWAGGER https://swagger.io/specification/
 const swaggerOptions = {
   swaggerDefinition: {
     info: {
-      title: 'Customer API',
-      description: 'Customer API Information',
+      title: 'Post API',
+      description: 'Post API Information',
       contact: {
         name: 'Sibo my brand'
       },
+      servers: ('http://localhost:3000')
     }
-  }
+  },
+  // ['.route/*.js']
+  apis: ('app.js')
 };
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+//SWAGGER ROUTES
+/** 
+ * @swagger
+ * /posts:
+ * GET:
+ *    description: use to request all Posts
+ *    responses:
+ *      '200':
+ *          description: A successful response
+*/
+app.get('/posts',(req,res) =>{
+  res.status(200).send('Posts Results');
+});
+
+/** 
+ * @swagger
+ * /post:
+ * GET A POST BY ID:
+ *    description: use to get a Post by Id
+ *    responses:
+ *      '200':
+ *          description: A successful response
+*/
+app.get('/posts/:postId',(req,res) =>{
+  res.status(200).send('Post Result');
+});
+
+/** 
+ * @swagger
+ * /post:
+ * PUT:
+ *    description: use to update a Post
+ *    responses:
+ *      '201':
+ *          description: A successful response
+*/
+app.put('/posts/:postId',(req,res) =>{
+  res.status(200).send('successfully updated post');
+});
+
+/** 
+ * @swagger
+ * /post:
+ * POST:
+ *    description: use to post a new Post
+ *    responses:
+ *      '200':
+ *          description: A successful response
+*/
+app.post('/posts',(req,res) =>{
+  res.status(200).send('successfully Posted');
+});
+
+/** 
+ * @swagger
+ * /post:
+ * DELETE:
+ *    description: use to delete a Post
+ *    responses:
+ *      '200':
+ *          description: A successful response
+*/
+app.delete('/posts/:postId',(req,res) =>{
+  res.status(200).send('successfully deleted');
+});
+
+
+
+
 
 //SUBMIT A POST
 router.post("/", verify, async (req, res) => {
